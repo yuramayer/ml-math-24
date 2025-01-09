9890a72cd8beda1e3917649b7a6f49d8824925bd
yuramayer <votetopovorot2015@gmail.com>
2025-01-09 18:08:05 +0300

    feat: created first hw

---
+++ hws/hw_1/ans/hw_1_ans.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+def check_task(your_answer, right_answer):
+    assert np.array_equal(your_answer, right_answer) 
+
+
+ans_1 = np.ndarray([40, 33,  1,  6, 20, 16])
+
+
