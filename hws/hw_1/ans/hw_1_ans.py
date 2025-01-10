0c67fd78995961e23d5d57ce41893bdf48236437
yuramayer <votetopovorot2015@gmail.com>
2025-01-10 12:43:02 +0300

    fix: updated hw 1, added another task

--- hws/hw_1/ans/hw_1_ans.py
+++ hws/hw_1/ans/hw_1_ans.py
@@ -5,6 +5,10 @@ def check_123(your_answer, right_answer):
     assert np.array_equal(your_answer, right_answer) 
 
 
+def check_4(your_answer, right_answer):
+    assert your_answer == right_answer 
+
 ans_1 = np.array([40, 33,  1,  6, 20, 16])
 ans_2 = np.array([ 5, 12, 10,  3,  6, 13,  8, 21,  5])
 ans_3 = np.array([13, 12, 15, 13, 17,  9, 20, 15, 25])
+ans_4 = 1348
