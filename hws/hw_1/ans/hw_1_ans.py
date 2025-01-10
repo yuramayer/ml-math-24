0202050116cde43ca76bce4e6b944300f1ccb4bc
yuramayer <votetopovorot2015@gmail.com>
2025-01-10 11:22:28 +0300

    feat: updated hw1

--- hws/hw_1/ans/hw_1_ans.py
+++ hws/hw_1/ans/hw_1_ans.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
 
-def check_task(your_answer, right_answer):
+def check_123(your_answer, right_answer):
     assert np.array_equal(your_answer, right_answer) 
 
 
 ans_1 = np.array([40, 33,  1,  6, 20, 16])
-
-
+ans_2 = np.array([ 5, 12, 10,  3,  6, 13,  8, 21,  5])
+ans_3 = np.array([13, 12, 15, 13, 17,  9, 20, 15, 25])
